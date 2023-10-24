d4f5d2eb4374acfb0ce895d02dc74a5cc8dea5b5
kimchaemin000617 <dktjdej25@gmail.com>
2023-10-24 22:21:40 +0900

    Django - 공통 모델 생성

--- back/config/attendance/models/common.py
+++ back/config/attendance/models/common.py
@@ -1,2 +1,21 @@
 from django.db import models
 
+
+class BaseModel(models.Model):
+    rdate = models.DateTimeField(
+        auto_now_add=True,
+        blank=True,
+        null=False,
+        verbose_name="생성일",
+        help_text="데이터 생성일"
+    )
+    mdate = models.DateField(
+        auto_now_add=True,
+        blank=True,
+        null=False,
+        verbose_name="수정일",
+        help_text="데이터 수정일"
+    )
+
+    class Meta:
+        abstract = True
